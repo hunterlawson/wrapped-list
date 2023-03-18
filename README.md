ab40cc355c512b514c3ec7cd787b91a84427edd8
Hunter Lawson <hunter.lawson@ufl.edu>
2023-03-18 06:50:04 -0400

    add docs

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # wrapped-list
 
-This crate provides the `wrapped_list!` macro which allows you to create a list of elements that are ped by an object, function, or macro at compile time.
+This crate provides the `wrapped_list!` macro which allows you to create a list of elements that are wrapped by an object, function, or another macro at compile time.
+
+```rust
+wrapped_list!(Box::new, value_1, value_2, ...)
+```
+
+Evaluates to:
+
+```rust
+[Box::new(value_1), Box::new(value_2), ...]
+```
+
+With this you can:
+
+- [Wrap values with a tuple struct or enum](#wrap-values-with-a-tuple-struct-or-enum)
+- [Wrap values with an object or function](#wrap-values-with-an-object-or-function)
+- [Wrap values with a macro](#wrap-values-with-a-macro)
 
 ## Examples
 
-### Wrap with a tuple struct or enum
+### Wrap values with a tuple struct or enum
 
 ```rust
-use wrapped_list::*;
+use wrapped_list::wrapped_list;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Wrapper(i32);
@@ -17,10 +33,10 @@ let wrapped_items = wrapped_list!(Wrapper; 1, 2, 3, 4);
 assert_eq!(wrapped_items, [Wrapper(1), Wrapper(2), Wrapper(3), Wrapper(4)]);
 ```
 
-### Wrap with an object or function
+### Wrap values with an object or function
 
 ```rust
-use wrapped_list::*;
+use wrapped_list::wrapped_list;
 
 let boxed_items = wrapped_list!(Box::new; 1, 2, 3);
 
@@ -28,7 +44,7 @@ assert_eq!(boxed_items, [Box::new(1), Box::new(2), Box::new(3)])
 ```
 
 ```rust
-use wrapped_list::*;
+use wrapped_list::wrapped_list;
 
 let func = |x| x * 2;
 
@@ -37,10 +53,10 @@ let doubled = wrapped_list!(func; 1, 2, 3);
 assert_eq!(doubled, [2, 4, 6]);
 ```
 
-### Wrap with a macro
+### Wrap values with a macro
 
 ```rust
-use wrapped_list::*;
+use wrapped_list::wrapped_list;
 
 macro_rules! add_one {
     ($e:expr) => {
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,82 @@
 //! # wrapped-list
-//! 
-//! This crate provides the `wrapped_list!` macro which allows you to create a list of elements that are wrapped by an object, function, or macro at compile time.
-//! 
-//! ## Examples
-//! 
-//! ### Wrap with a tuple struct or enum
-//! 
+//!
+//! This crate provides the `wrapped_list!` macro which allows you to create a list of elements 
+//! that are wrappped by an object, function, or another macro at compile time.
+//!
+//! ```ignore
+//! wrapped_list!(Box::new, value_1, value_2, ...)
 //! ```
-//! use wrapped_list::*;
-//! 
+//!
+//! Evaluates to:
+//!
+//! ```ignore
+//! [Box::new(value_1), Box::new(value_2), ...]
+//! ```
+//!
+//! With this you can:
+//!
+//! - [Wrap values with a tuple struct or enum](#wrap-values-with-a-tuple-struct-or-enum)
+//! - [Wrap values with an object or function](#wrap-values-with-an-object-or-function)
+//! - [Wrap values with a macro](#wrap-values-with-a-macro)
+//!
+//! ## Examples
+//!
+//! ### Wrap values with a tuple struct or enum
+//!
+//! ```
+//! use wrapped_list::wrapped_list;
+//!
 //! #[derive(Debug, PartialEq, Eq)]
 //! struct Wrapper(i32);
-//! 
+//!
 //! let wrapped_items = wrapped_list!(Wrapper; 1, 2, 3, 4);
-//! 
+//!
 //! assert_eq!(wrapped_items, [Wrapper(1), Wrapper(2), Wrapper(3), Wrapper(4)]);
 //! ```
-//! 
-//! ### Wrap with an object or function
-//! 
+//!
+//! ### Wrap values with an object or function
+//!
 //! ```
-//! use wrapped_list::*;
-//! 
+//! use wrapped_list::wrapped_list;
+//!
 //! let boxed_items = wrapped_list!(Box::new; 1, 2, 3);
-//! 
+//!
 //! assert_eq!(boxed_items, [Box::new(1), Box::new(2), Box::new(3)])
 //! ```
-//! 
+//!
 //! ```
-//! use wrapped_list::*;
-//! 
+//! use wrapped_list::wrapped_list;
+//!
 //! let func = |x| x * 2;
-//! 
+//!
 //! let doubled = wrapped_list!(func; 1, 2, 3);
-//! 
+//!
 //! assert_eq!(doubled, [2, 4, 6]);
 //! ```
-//! 
-//! ### Wrap with a macro
-//! 
+//!
+//! ### Wrap values with a macro
+//!
 //! ```
-//! use wrapped_list::*;
-//! 
+//! use wrapped_list::wrapped_list;
+//!
 //! macro_rules! add_one {
 //!     ($e:expr) => {
 //!         $e + 1
 //!     };
 //! }
-//! 
+//!
 //! let one_more = wrapped_list!(add_one!; 1, 2, 3);
-//! 
+//!
 //! assert_eq!(one_more, [2, 3, 4]);
 //! ```
 
+/// Macro to wrap a list of values with a function, object, or another macro.
+/// 
+/// 
+/// 
+/// See the [examples] to learn more.
+/// 
+/// [examples]: crate#examples
 #[macro_export]
 macro_rules! wrapped_list {
     /* ---------------------------------- Paths --------------------------------- */
@@ -63,11 +87,11 @@ macro_rules! wrapped_list {
         [$wrapper($e1), $wrapper($e2)]
     };
     ($wrapper:path ; $e1:expr, $e2:expr, $($es:expr),*) => {
-        __wrapped_list_impl!($wrapper ; [$wrapper($e1)] ; $e2 ; $($es),*)
+        $crate::__wrapped_list_impl!($wrapper ; [$wrapper($e1)] ; $e2 ; $($es),*)
     };
     // Allow trailing commas
     ($wrapper:path ; $e1:expr, $e2:expr, $($es:expr,)*) => {
-        wrapped_list!($wrapper ; $e1 , $e2 , $($es),*)
+        $crate::wrapped_list!($wrapper ; $e1 , $e2 , $($es),*)
     };
     /* --------------------------------- Macros --------------------------------- */
     ($wrapper:ident! ; $e:expr $(,)?) => {
@@ -77,14 +101,16 @@ macro_rules! wrapped_list {
         [$wrapper!($e1), $wrapper!($e2)]
     };
     ($wrapper:ident! ; $e1:expr, $e2:expr, $($es:expr),*) => {
-        __wrapped_list_impl!($wrapper! ; [$wrapper!($e1)] ; $e2 ; $($es),*)
+        $crate::__wrapped_list_impl!($wrapper! ; [$wrapper!($e1)] ; $e2 ; $($es),*)
     };
     // Allow trailing commas
     ($wrapper:ident! ; $e1:expr, $e2:expr, $($es:expr,)*) => {
-        wrapped_list!($wrapper! ; $e1 , $e2 , $($es),*)
+        $crate::wrapped_list!($wrapper! ; $e1 , $e2 , $($es),*)
     };
 }
 
+/// Internal implementation macro used by [wrapped_list].
+/// This is not intended to be called on its own.
 #[macro_export]
 macro_rules! __wrapped_list_impl {
     /* ---------------------------------- Paths --------------------------------- */
@@ -92,25 +118,23 @@ macro_rules! __wrapped_list_impl {
         $out
     };
     ($wrapper:path ; [$($out:tt)*] ; $e1:expr ; $e2:expr, $($es:expr),*) => {
-        __wrapped_list_impl!($wrapper ; [$($out)* , $wrapper($e1)] ; $e2 ; $($es),*)
+        $crate::__wrapped_list_impl!($wrapper ; [$($out)* , $wrapper($e1)] ; $e2 ; $($es),*)
     };
     ($wrapper:path ; [$($out:tt)*] ; $e1:expr ; $e2:expr) => {
-        __wrapped_list_impl!([$($out)* , $wrapper($e1), $wrapper($e2)])
+        $crate::__wrapped_list_impl!([$($out)* , $wrapper($e1), $wrapper($e2)])
     };
     /* --------------------------------- Macros --------------------------------- */
     ($wrapper:ident! ; [$($out:tt)*] ; $e1:expr ; $e2:expr, $($es:expr),*) => {
-        __wrapped_list_impl!($wrapper! ; [$($out)* , $wrapper!($e1)] ; $e2 ; $($es),*)
+        $crate::__wrapped_list_impl!($wrapper! ; [$($out)* , $wrapper!($e1)] ; $e2 ; $($es),*)
     };
     ($wrapper:ident! ; [$($out:tt)*] ; $e1:expr ; $e2:expr) => {
-        __wrapped_list_impl!([$($out)* , $wrapper!($e1), $wrapper!($e2)])
+        $crate::__wrapped_list_impl!([$($out)* , $wrapper!($e1), $wrapper!($e2)])
     };
 }
-
 
 #[cfg(test)]
 mod tests {
     use duplicate::duplicate_item;
-
 
     #[derive(PartialEq, Eq, Debug)]
     struct Wrapper(i32);
@@ -149,7 +173,7 @@ mod tests {
     macro_rules! wrapper_macro2 {
         ($e:expr) => {
             Wrapper(add_one!($e))
-        }
+        };
     }
 
     #[duplicate_item(
@@ -197,7 +221,12 @@ mod tests {
         assert_eq!(my_list, wrapped_list!(macro_name!; 1, 2));
         let my_list = [macro_name!(1), macro_name!(2), macro_name!(3)];
         assert_eq!(my_list, wrapped_list!(macro_name!; 1, 2, 3));
-        let my_list = [macro_name!(1), macro_name!(2), macro_name!(3), macro_name!(4)];
+        let my_list = [
+            macro_name!(1),
+            macro_name!(2),
+            macro_name!(3),
+            macro_name!(4),
+        ];
         assert_eq!(my_list, wrapped_list!(macro_name!; 1, 2, 3, 4));
     }
 
@@ -209,7 +238,12 @@ mod tests {
         assert_eq!(my_list, wrapped_list!(wrapper_macro2!; 1, 2,));
         let my_list = [wrapper_macro2!(1), wrapper_macro2!(2), wrapper_macro2!(3)];
         assert_eq!(my_list, wrapped_list!(wrapper_macro2!; 1, 2, 3,));
-        let my_list = [wrapper_macro2!(1), wrapper_macro2!(2), wrapper_macro2!(3), wrapper_macro2!(4)];
+        let my_list = [
+            wrapper_macro2!(1),
+            wrapper_macro2!(2),
+            wrapper_macro2!(3),
+            wrapper_macro2!(4),
+        ];
         assert_eq!(my_list, wrapped_list!(wrapper_macro2!; 1, 2, 3, 4,));
     }
 }
